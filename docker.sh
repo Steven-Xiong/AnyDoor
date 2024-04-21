9d7d6358d2fcf2cc5517a0bc8776789bde7a25c8
Steven-Xiong <x.zhexiao@wustl.edu>
2024-04-21 11:21:52 -0500

    4.21

--- docker.sh
+++ docker.sh
@@ -1,12 +1,12 @@
-# BSUB -o ./bjob_logs/train_4.16_tsv_concatImageTxt_objstxtembedding_grad_fixed_VG_ref_zero.%J
+# BSUB -o ./bjob_logs/train_4.21_tsv_concatImageTxt_objstxtembedding_grad_fixed_3data_2306.%J
 
 # BSUB -q gpu-compute
-# BSUB -m "a100s-2305.engr.wustl.edu" 
+# BSUB -m "a100s-2306.engr.wustl.edu" 
 
-# BSUB -gpu "num=4:mode=shared:j_exclusive=yes:gmodel=NVIDIAA100_SXM4_80GB" 
+# BSUB -gpu "num=3:mode=shared:j_exclusive=yes:gmodel=NVIDIAA100_SXM4_80GB" 
 # BSUB -J anydoor
 
 source ~/.bashrc
 conda activate anydoor
 cd /project/osprey/scratch/x.zhexiao/edit/LayoutBooth/AnyDoor
-sh scripts/train.sh
+python train_all_3cards.py
